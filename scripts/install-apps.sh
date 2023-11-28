d54612d870cd74ec4269c9a476c8121d3424af59
igne.jone <igne@jone>
2023-11-28 15:30:35 +0200

    update names

--- scripts/install-apps.sh
+++ scripts/install-apps.sh
@@ -6,9 +6,10 @@ sudo apt update -y
 
 # Install unzip, git, and Python (if not already installed)
 sudo apt install -y unzip git python3 python3-pip
+pip install git-remote-codecommit --force --user
 
 # Download and install Terraform (linux_386)
-sudo rm /usr/local/bin/terraform
+#sudo rm /usr/local/bin/terraform
 curl -s -qL -o terraform.zip https://releases.hashicorp.com/terraform/1.6.4/terraform_1.6.4_linux_386.zip
 unzip -o terraform.zip
 sudo mv terraform /bin
@@ -18,5 +19,8 @@ rm terraform.zip
 echo "unzip version:"
 
 #Configure git credentials
-git config --global credential.helper '!aws codecommit credential-helper $@'
+
+git config --global credential.helper '!aws --profile dev-mfa codecommit credential-helper $@'
 git config --global credential.UseHttpPath true
+
+git clone --branch d4ml-iac-terraform-igne https://git-codecommit.eu-west-1.amazonaws.com/v1/repos/d4ml-iac-terraform-skillboost
